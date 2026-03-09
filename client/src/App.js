fd58327bc38794f005b86d2728f67fd70ee960e7
DaniDev937 <daniel_rebudiao@dlsu.edu.ph>
2026-03-10 02:46:33 +0800

    routed postservice

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './loginPageFront/LoginPage';
 import Homepage from './homePageFront/HomePage';
+import PostService from './postServiceFront/PostService';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -21,6 +22,7 @@ function App() {
           path="/homepage" 
           element={isAuthenticated  ? <Homepage /> : <Navigate to="/" />} 
         />
+        <Route path="/postservice" element={<PostService />} />
       </Routes>
     </Router>
   );
