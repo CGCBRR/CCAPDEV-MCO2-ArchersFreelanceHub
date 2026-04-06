e58fa81c0ad8ea094e06ca40985ce71b384f4f6c
DaniDev937 <daniel_rebudiao@dlsu.edu.ph>
2026-04-07 00:02:57 +0800

    fixed loading

--- client/src/homePageFront/HomePage.js
+++ client/src/homePageFront/HomePage.js
@@ -153,6 +153,8 @@ const Homepage = () => {
         } catch (fallbackErr) {
           console.error("Error fetching fallback categories:", fallbackErr);
         }
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchCategories();
@@ -257,6 +259,8 @@ const Homepage = () => {
         console.log("Fetched user profile:", res.data);
       } catch (err) {
         console.error("Error fetching user profile:", err);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchUserProfile();
@@ -272,6 +276,8 @@ const Homepage = () => {
         console.log("Fetched user statistics:", res.data);
       } catch (err) {
         console.error("Error fetching statistics:", err);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchStatistics();
@@ -287,7 +293,9 @@ const Homepage = () => {
         console.log("Fetched freelancer:", rest.data);
         } catch (err) {
         console.error("Error fetching freelancers:", err);
-        }
+        } finally {
+        setIsLoading(false);
+      }
     };
     fetchFreelancers();
 
@@ -302,7 +310,9 @@ const Homepage = () => {
         console.log("Fetched services:", rest.data);
         } catch (err) {
         console.error("Error fetching services:", err);
-        }
+        } finally {
+        setIsLoading(false);
+      }
     };
     fetchServices();
 
